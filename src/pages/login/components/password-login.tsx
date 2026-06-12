c7dffea82aaf57a66daac75fbeeeddcab2383ee1
kun <weikun@weikundeMacBook-Air.local>
2026-06-12 11:56:34 +0800

    修改title

--- src/pages/login/components/password-login.tsx
+++ src/pages/login/components/password-login.tsx
@@ -31,11 +31,8 @@ export default function PasswordLogin() {
     <>
       <Space orientation="vertical">
         <h2 className="text-colorText mb-3 text-3xl font-bold leading-9 tracking-tight lg:text-4xl">
-          {t("authority.welcomeBack")}
+          {t("authority.title")}
         </h2>
-        <p className="lg:text-base text-sm text-colorTextSecondary">
-          {t("authority.loginDescription")}
-        </p>
       </Space>
 
       <Form
