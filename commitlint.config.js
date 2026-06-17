4bbfed7697890f3f0c424e918f8ea1bf5f0bc92e
kun <weikun@weikundeMacBook-Air.local>
2026-06-17 17:30:36 +0800

    添加主题色切

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,10 +1,5 @@
 export default {
 	rules: {
 		"body-leading-blank": [2, "always"],
-		"footer-leading-blank": [1, "always"],
-		"header-max-length": [2, "always", 108],
-		"body-max-line-length": [2, "always", 1000],
-		"subject-empty": [2, "never"],
-		"type-empty": [2, "never"],
 	},
 };
