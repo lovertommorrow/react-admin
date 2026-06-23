824f1cd15955e9817e157de9ce808eaed26c25db
kun <weikun@weikundeMacBook-Air.local>
2026-06-23 10:36:15 +0800

    移除代码

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './i18n'
 import './styles/index.css'
-import App from './App.tsx'
 import LocaleProvider from './providers/LocaleProvider.tsx'
 
 async function setupApp() {
